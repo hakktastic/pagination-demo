1c56b5ca0b4582dece77cd315dab7742b2907b88
hakktastic <hakktastic@gmail.com>
2026-01-19 17:01:59 +0100

    fix response code from 404 to 400
    -

--- src/main/java/nl/hakktastic/pagination_demo/DemoRestControllerAdvice.java
+++ src/main/java/nl/hakktastic/pagination_demo/DemoRestControllerAdvice.java
@@ -14,13 +14,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class DemoRestControllerAdvice {
 
-  private static final URI URI_PROBLEM_TYPE_ENTITY_NOT_FOUND = URI.create("https://dummy.example.com/problems/entity-not-found");
+  private static final URI URI_PROBLEM_TYPE_ENTITY_BAD_REQUEST = URI.create("https://dummy.example.com/problems/bad-request");
 
   @ExceptionHandler(IllegalArgumentException.class)
   ProblemDetail handleBadRequest(IllegalArgumentException illegalArgumentException, HttpServletRequest httpServletRequest) {
 
-    val problem = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
-    problem.setType(URI_PROBLEM_TYPE_ENTITY_NOT_FOUND);
+    val problem = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+    problem.setType(URI_PROBLEM_TYPE_ENTITY_BAD_REQUEST);
     problem.setTitle("Illegal Argument");
     problem.setDetail(illegalArgumentException.getMessage());
     problem.setInstance(URI.create(httpServletRequest.getRequestURI()));
